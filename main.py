5152aa8829eb82bc40f7e729ff14f6d99d7cedd7
M1nZof <maxykl38@gmail.com>
2023-02-04 13:49:23 +0300

    Added ifmain construction

--- main.py
+++ main.py
@@ -7,31 +7,32 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from collections import defaultdict
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+if __name__ == '__main__':
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-template = env.get_template('template.html')
+    template = env.get_template('template.html')
 
-excel_wine = pandas.read_excel('wine3.xlsx', na_values=None, keep_default_na=False).to_dict(orient='records')
+    excel_wine = pandas.read_excel('wine3.xlsx', na_values=None, keep_default_na=False).to_dict(orient='records')
 
-sorted_excel_wine = defaultdict(list)
+    sorted_excel_wine = defaultdict(list)
 
-for product in excel_wine:
-    sorted_excel_wine[product['Категория']].append(product)
+    for product in excel_wine:
+        sorted_excel_wine[product['Категория']].append(product)
 
-winery_age = get_winery_age()
-winery_age_form = get_winery_age_form(winery_age)
+    winery_age = get_winery_age()
+    winery_age_form = get_winery_age_form(winery_age)
 
-rendered_page = template.render(
-    winery_age=winery_age,
-    winery_age_form=winery_age_form,
-    drinks=sorted_excel_wine
-)
+    rendered_page = template.render(
+        winery_age=winery_age,
+        winery_age_form=winery_age_form,
+        drinks=sorted_excel_wine
+    )
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
