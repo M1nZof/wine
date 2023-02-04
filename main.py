2629c7661a86edb9449d219b09c91b3f6d25c5c0
M1nZof <maxykl38@gmail.com>
2023-02-04 14:08:52 +0300

    Added arguments to start project
    - path - path to .xlsx with drinks data;
    - name - file name of .xlsx

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import datetime
 import pandas
+import argparse
+import os
 
 from age_helpers import get_winery_age, get_winery_age_form
 from http.server import HTTPServer, SimpleHTTPRequestHandler
@@ -8,6 +10,15 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from collections import defaultdict
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--path',
+                        help='Путь до .xlsx файла. По умолчанию в корне программы',
+                        default='')
+    parser.add_argument('-n', '--name',
+                        help='Название .xlsx файла. Указывается с форматом файла. По умолчанию - wine.xlsx',
+                        default='wine.xlsx')
+    args = parser.parse_args()
+    
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
@@ -15,7 +26,7 @@ if __name__ == '__main__':
 
     template = env.get_template('template.html')
 
-    excel_wine = pandas.read_excel('wine3.xlsx', na_values=None, keep_default_na=False).to_dict(orient='records')
+    excel_wine = pandas.read_excel(os.path.join(args.path, args.name), na_values=None, keep_default_na=False).to_dict(orient='records')
 
     sorted_excel_wine = defaultdict(list)
 
